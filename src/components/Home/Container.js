a9e4f9dc54c1dd07dab7875a722297a35f2502e3
Angel Torres Sanchez <97angeltorres@gmail.com>
2019-03-21 21:55:10 +0100

    [ADD]Container

--- src/components/Home/Container.js
+++ src/components/Home/Container.js
@@ -11,58 +11,66 @@ import {
 
 import "./container-style.css";
 
+const myKawaiiInLeft = [
+  isLeft => (
+    <Cat size={320} mood={isLeft ? "excited" : "sad"} color="#596881" />
+  ),
+  isLeft => (
+    <File size={200} mood={isLeft ? "excited" : "sad"} color="#83D1FB" />
+  ),
+  isLeft => (
+    <IceCream
+      size={300}
+      mood={isLeft ? "excited" : "sad"}
+      color="#FDA7DC"
+    />
+  ),
+  isLeft => (
+    <Planet
+      size={220}
+      mood={isLeft ? "excited" : "sad"}
+      color="#FCCB7E"
+    />
+  )];
+
+const myKawaiiInRight = [
+  isLeft => (
+    <Browser
+      size={200}
+      mood={isLeft ? "sad" : "excited"}
+      color="#61DDBC"
+    />
+  ),
+  isLeft => (
+    <CreditCard
+      size={200}
+      mood={isLeft ? "sad" : "excited"}
+      color="#83D1FB"
+    />
+  )];
+
 class Container extends Component {
   constructor() {
     super();
 
     this.state = {
-      myKawaiiInLeft: [
-        isLeft => (
-          <Cat size={320} mood={isLeft ? "excited" : "sad"} color="#596881" />
-        ),
-        isLeft => (
-          <File size={200} mood={isLeft ? "excited" : "sad"} color="#83D1FB" />
-        ),
-        isLeft => (
-          <IceCream
-            size={300}
-            mood={isLeft ? "excited" : "sad"}
-            color="#FDA7DC"
-          />
-        ),
-        isLeft => (
-          <Planet
-            size={220}
-            mood={isLeft ? "excited" : "sad"}
-            color="#FCCB7E"
-          />
-        )
-      ],
-      myKawaiiInRight: [
-        isLeft => (
-          <Browser
-            size={200}
-            mood={isLeft ? "sad" : "excited"}
-            color="#61DDBC"
-          />
-        ),
-        isLeft => (
-          <CreditCard
-            size={200}
-            mood={isLeft ? "sad" : "excited"}
-            color="#83D1FB"
-          />
-        )
-      ]
+      myKawaiiInLeft,
+      myKawaiiInRight
     };
   }
 
+  reset = () => {
+
+    this.setState({ myKawaiiInLeft, myKawaiiInRight });
+
+  }
+  
   render() {
     const { myKawaiiInLeft, myKawaiiInRight } = this.state;
 
     return (
       <div>
-        <Switch disabled={true} />
+        <Switch onChange={this.reset} />
         <h2>Make kawaiis happy</h2>
 
         <div className="container-parent">
@@ -126,7 +134,7 @@ class Container extends Component {
           </div>
         </div>
       </div>
-    );
+    );//End Return
   }
 }
 
